6f0a28494d8884a03490bc7bb1a41466131f5802
TalkingCoffeePot <barrberry666@gmail.com>
2024-02-02 03:27:27 +0500

    On branch master  Changes to be committed: 	modified:   my_json/views.py 	modified:   webapp/templates/index.html
        Добавлен вывод ответа или ошибки пользователю

--- source/my_json/views.py
+++ source/my_json/views.py
@@ -1,15 +1,18 @@
 from django.shortcuts import render
 from django.views.generic import View
 import json
+from django.utils.decorators import method_decorator
 from django.http import JsonResponse
-from django.views.decorators.csrf import ensure_csrf_cookie
+from django.views.decorators.csrf import csrf_exempt
 # Create your views here.
 
 def sign_choice(path, a, b):
+    a, b = int(a), int(b)
     answer = 0
+    
     if 'add' in path:
         answer = a+b
-    elif 'subtract' in path:
+    elif 'substract' in path:
         answer = a-b
     elif 'multiply' in path:
         answer = a*b
@@ -20,10 +23,11 @@ def sign_choice(path, a, b):
             answer = 'Divided by Zero'
     return answer
 
+
+@method_decorator(csrf_exempt, name='dispatch')
 class MathView(View):
     def post(self, request, *args, **kwargs):
         answer = {}
-        print(request.body)
         if request.body:
             data = json.loads(request.body)
             
